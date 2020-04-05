1777aec7d1e98d7ea48b7407728f04e9a217de72
Brandon Dusch <brandondusch@gmail.com>
2020-04-05 12:28:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,7 @@ class Dog
   def self.new_from_db(row) 
     # returns an array representing the newly created Dog data
     dog = self.new(id: row[0], name: row[1], breed: row[2])
-    dog
+
     
   end
 end
