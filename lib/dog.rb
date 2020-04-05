71c358ff16fc0a6d35bcf0f5671eead111e0751b
Brandon Dusch <brandondusch@gmail.com>
2020-04-05 12:19:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,61 @@
+require 'pry'
+
+class Dog 
+  attr_accessor :name, :breed 
+  attr_reader :id
+  
+  def initialize(id: id = nil, name: , breed: )
+    @id = id
+    @name = name 
+    @breed = breed
+  end
+  
+  def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
+  def self.drop_table 
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+    if(self.id)
+      self.update 
+    else 
+      sql = <<-SQL 
+        INSERT INTO dogs (name, breed)
+        VALUES(?, ?);
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
+  end
+  
+  def self.create(id:, name:, breed: )
+    dog = Dog.new(id, name, breed)
+    dog.save 
+    dog
+  end
+
+  
+  def update
+    sql = <<-SQL 
+      UPDATE dogs 
+      SET name = ?, breed = ?
+      WHERE id = ?;
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+end
