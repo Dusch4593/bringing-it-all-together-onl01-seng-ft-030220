3f1bafcd09759b0241e2c2ee15689f8a47501eda
Brandon Dusch <brandondusch@gmail.com>
2020-04-05 12:30:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,15 @@ class Dog
   def self.new_from_db(row) 
     # returns an array representing the newly created Dog data
     dog = self.new(id: row[0], name: row[1], breed: row[2])
-
-    
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * 
+      FROM dogs 
+      WHERE dogs.id = ?;
+    SQL
+    dog = DB[:conn].execute(sql, id)
+    binding.pry
   end
 end
